c7d3bca5b74f5f292218d76bb8ee6e5b6af81914
Zhen Cui <zcui1989@gmail.com>
2019-09-21 01:27:27 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const lines = document.querySelector('.lines');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    lines.addEventListener('click', () =>{
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+    });
+
+        //Lines animation
+        lines.classList.toggle('toggle');
+    });
+}
+
+
+navSlide();
